b934b74305aa39f53204c0d2d9bb6c47ab3a2d79
calebesg <calebeguimaraes10@gmail.com>
2026-02-16 15:08:04 -0300

    create cetificado conteudo

--- src/app/pages/certificado/certificado.ts
+++ src/app/pages/certificado/certificado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 
 @Component({
   selector: 'app-certificado',
-  imports: [],
+  imports: [SecondaryButton],
   templateUrl: './certificado.html',
   styleUrl: './certificado.css',
 })
