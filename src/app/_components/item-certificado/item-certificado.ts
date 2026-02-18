e574b4e7c78ba200fd58159b60ad3552f903b475
calebesg <calebeguimaraes10@gmail.com>
2026-02-18 11:05:56 -0300

    create navigation

--- src/app/_components/item-certificado/item-certificado.ts
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../secondary-button/secondary-button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.css',
 })
-export class ItemCertificado {}
+export class ItemCertificado {
+  id: Number = 1;
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
-import { PrimaryButton } from './_components/primary-button/primary-button';
-import { SecondaryButton } from './_components/secondary-button/secondary-button';
-import { ItemCertificado } from './_components/item-certificado/item-certificado';
+
 import { BaseUi } from './_components/base-ui/base-ui';
-import { Certificados } from './pages/certificados/certificados';
-import { CertificadoForm } from './pages/certificado-form/certificado-form';
-import { Certificado } from './pages/certificado/certificado';
+
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    Navbar,
-    CommonModule,
-    PrimaryButton,
-    SecondaryButton,
-    ItemCertificado,
-    BaseUi,
-    Certificados,
-    CertificadoForm,
-    Certificado,
-  ],
+  imports: [Navbar, CommonModule, BaseUi, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
