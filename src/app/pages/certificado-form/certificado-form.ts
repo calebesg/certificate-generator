c3bcfbb8b3917fcdee644c35bbc6d590ac61c4f2
calebesg <calebeguimaraes10@gmail.com>
2026-02-18 12:16:24 -0300

    implemented ngform

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css',
 })
-export class CertificadoForm {}
+export class CertificadoForm {
+  name: string = '';
+  activity: string = '';
+  activities: string[] = ['Angular', 'React'];
+}
